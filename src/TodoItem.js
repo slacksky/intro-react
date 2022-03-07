68aaba2161a4551f7c79b510478e536d9f4198a5
Jorge Vivas <slacksky@gmail.com>
2022-03-07 18:43:20 -0500

    added interactivity and on change events

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,31 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+
+  const onComplete = () => {
+    alert('Ya completaste el TODO ' + props.text);
+  }; 
+
+  const onDelete = () => {
+    alert('Ya borraste el TODO ' + props.text);
+  }; 
+
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span 
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onComplete}
+      >
         √
       </span>
-      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}
+      >
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span 
+        className="Icon Icon-delete"
+        onClick={onDelete}
+      >
         X
       </span>
     </li>
