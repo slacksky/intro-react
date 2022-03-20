992d47150ab6a6e09f2f52c6c1ddacaf6e562134
Jorge Vivas <slacksky@gmail.com>
2022-03-20 16:40:32 -0500

    added persisntence  in local storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { TodoCounter } from './TodoCounter'
-import { TodoSearch } from "./TodoSearch";
-import { TodoItem } from "./TodoItem";
-import { TodoList } from "./TodoList";
-import { CreateTodoButton } from "./CreateTodoButton";
+
+import { AppUI } from "./AppUI";
+
 //import logo from './logo.svg';
 //import './App.css';
 
-const defaultTodos= [
+/*const defaultTodos= [
   {text: 'trabajar en el AzureAngel', completed: false},
   {text: 'tunear  motor electrico', completed: false},
   {text: 'instalar extensor de rango', completed: false}
-];
+];*/
 
 
 function App() {
-  const [todos, setTodos]= React.useState(defaultTodos); 
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos = [];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+
+  const [todos, setTodos]= React.useState(parsedTodos); 
   const  [searchValue, setSearchValue] = React.useState('');
   
   const completedTodos = todos.filter(todo => !!todo.completed ).length;
@@ -36,18 +46,18 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const   completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     
     const newTodos = [...todos];//injected todo array?
     newTodos[todoIndex].completed = true; //shorter way
-    setTodos(newTodos);
-
-
-    //todos[todoIndex] = {
-    //  text: todos[todoIndex].text,
-    //  completed: true,
-    //};//alternative way to find completed todos 
+    saveTodos(newTodos);
   };
 
 
@@ -56,37 +66,19 @@ function App() {
     
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1); 
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
-    <React.Fragment>
-    
-      <TodoCounter 
-        total={totalTodos}
-        completed={completedTodos}
+    <AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
       />
-    
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      
-    
-      <TodoList>
-      {searchedTodos.map(todo => (
-        <TodoItem 
-        key={todo.text} 
-        text={todo.text}
-        completed={todo.completed} 
-        onComplete={() => completeTodo(todo.text)}
-        onDelete={() => deleteTodo(todo.text)}
-      />
-      ))}
-      </TodoList>
-
-   <CreateTodoButton />
-    </React.Fragment>
   );
 }
 
